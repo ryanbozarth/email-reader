0b13cc82caa4e0207563ba6006ed8693a174b163
Ryan Bozarth <ryan_bozarth@ryan-bozarth.local>
2017-02-13 14:12:50 -0800

    made mailbox list component dynamic

--- js/components/mailbox-list.js
+++ js/components/mailbox-list.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import {Link} from 'react-router'
 
+const MailboxLink = ({type}) => (
+  <li>
+    <Link to={`mailbox-${type}`}>{type}</Link>
+  </li>
+);
 
-export default function MailboxList(props) {
-
-  const mailboxes = Object.keys(props.emails).map((mailboxType, index) => {
-    console.log(mailboxType)
-    return (
-      <ul>
-        <li><Link to={mailboxType.[index]} key={index}>{mailboxType.[index]}</Link></li>
-        <li><Link to="mailbox-2">Mailbox Two</Link></li>
-      </ul>
+export default function MailboxList({emails}) {
+  const mailboxes = Object.keys(emails);
+  const html = mailboxes.map((type, index) => (
+      <MailboxLink key={type} type={type} />
     )
-  });
+  );
+  return (
+    <ul>
+      {html}
+    </ul>
+  )
 }
